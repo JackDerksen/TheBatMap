e1eceda249bd7d19b8d1bfa863d054ff7673e0c4
Jack Derksen <jmderksen9@gmail.com>
2025-03-18 13:11:04 -0600

    Fixed project structure, should work now!

---
+++ src/main/java/ca/macewan/thebatmap/TheBatMapApplication.java
@@ -0,0 +1,9 @@
+package ca.macewan.thebatmap;
+
+/**
+ * Base package class for TheBatMap application.
+ */
+public class TheBatMapApplication {
+    // Can be empty or contain application-wide constants or utility methods.
+    // We can keep it empty for now, it just fixes some errors.
+}
